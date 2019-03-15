3999702512399e5352647d4183f09e9a9e7f511e
Bo Henderson <bohende@gmail.com>
2019-03-15 14:48:05 +0530

    move examples into src

--- scripts/fill-form.py
+++
@@ -1,8 +0,0 @@
- #!/usr/bin/env python
-from fdfgen import forge_fdf
- 
-fields = [('topmostSubform[0].Page1[0].f1_02[0]', 'Batman')]
-fdf = forge_fdf("",fields,[],[],[])
-
-with open("build/fdf/f1040.fdf", "wb") as fdf_file:
-   fdf_file.write(fdf)
