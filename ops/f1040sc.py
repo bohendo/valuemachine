2cf6a68966ea0fce5ee6ec5597eff1c9fd668b72
Bo Henderson <bohende@gmail.com>
2019-04-11 13:24:35 +0530

    finish f1040sc builder

--- ops/f1040sc.py
+++ ops/f1040sc.py
@@ -3,11 +3,31 @@ import csv
 import json
 import sys
 
+########################################
+# Read data from input files
+
 income=json.load(open(sys.argv[1]))
 f1040=json.load(open(sys.argv[2]))
 f1040sc=json.load(open(sys.argv[3]))
 txHistory = csv.DictReader(open(sys.argv[4], 'rb'))
 target=sys.argv[5]+'/f1040sc.json'
+
+########################################
+# Helper Functions
+
+def fromForm(dollars, cents):
+  number = ''
+  number += '0' if str(dollars) == "" else str(dollars)
+  number += '.'
+  number += '0' if str(cents) == "" else str(cents)
+  return float(number)
+
+def toForm(num, isCents):
+  out = str(round(num, 2)).split('.')[isCents]
+  return '' if out == '0' else out
+
+########################################
+# Build the form
 
 f1040sc['FullName'] = f1040['FirstNameAndInitial'] + ' ' + f1040['LastName']
 f1040sc['SocialSecurityNumber'] = f1040['SocialSecurityNumber']
@@ -34,9 +54,50 @@ for row in txHistory:
     print('Got full payment of %s by %s' % (row['value_in'], row['from']))
     total_income += float(row['value_in'])
 
-f1040sc['Line1'] = str(round(total_income,2)).split('.')[0]
-f1040sc['Line1c'] = str(round(total_income,2)).split('.')[1]
+f1040sc['Line1'] = toForm(total_income, 0)
+f1040sc['Line1c'] = toForm(total_income, 1)
 print('Total income: %s' % (total_income))
+
+returns = fromForm(f1040sc['Line2'], f1040sc['Line2c'])
+
+line3 = total_income - returns
+f1040sc['Line3'] = toForm(line3, 0)
+f1040sc['Line3c'] = toForm(line3, 1)
+
+line4 = fromForm(f1040sc['Line42'], f1040sc['Line42c'])
+f1040sc['Line4'] = toForm(line4, 0)
+f1040sc['Line4c'] = toForm(line4, 1)
+
+line5 = line3 - line4
+f1040sc['Line5'] = toForm(line5, 0)
+f1040sc['Line5c'] = toForm(line5, 1)
+
+line6 = fromForm(f1040sc['Line6'], f1040sc['Line6c'])
+line7 = line6 + line5
+f1040sc['Line7'] = toForm(line7, 0)
+f1040sc['Line7c'] = toForm(line7, 1)
+
+total_expenses = 0
+for expense in income['expenses']:
+  if expense in f1040sc:
+    amount = float(income['expenses'][expense])
+    print('found expense for %s: %d' % (expense, amount))
+    f1040sc[expense] = toForm(amount, 0)
+    f1040sc[expense + 'c'] = toForm(amount, 1)
+    total_expenses += amount
+
+f1040sc['Line28'] = toForm(total_expenses, 0)
+f1040sc['Line28c'] = toForm(total_expenses, 1)
+
+line29 = line7 - total_expenses
+f1040sc['Line29'] = toForm(line29, 0)
+f1040sc['Line29c'] = toForm(line29, 1)
+
+line30 = fromForm(f1040sc['Line30'], f1040sc['Line30c'])
+
+line31 = line29 - line30
+f1040sc['Line31'] = toForm(line31, 0)
+f1040sc['Line31c'] = toForm(line31, 1)
 
 with open(target, "wb") as output:
   json.dump(f1040sc, output)
