be4419b0bae8b0b87c0050b83119bd3304dc6895
Bo Henderson <bohende@gmail.com>
2019-12-22 19:51:00 +0530

    fix fetcher & commit copy of empty forms/fields

--- ops/fetch.sh
+++ ops/fetch.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 set -e
 
-source="${1:-federal}"
-name="${2}"
-forms="build/forms"
+name="${1}"
+forms="ops/forms"
+
+mkdir -p $forms
 
 if [[ -n "$name" ]]
 then
-  url="`cat ops/sources/$source.json | jq '.data | map(select(.name == "'$name'"))[0].url' | tr -d '"'`"
-
-  echo "curl -s $url > $forms/$name.pdf"
-  curl -s $url > $forms/$name.pdf
-
-  echo "pdftk $forms/$name.pdf dump_data_fields > $forms/$name.fields"
-  pdftk $forms/$name.pdf dump_data_fields > $forms/$name.fields
-
+	wget "https://www.irs.gov/pub/irs-pdf/$name.pdf" --output-document="$forms/$name.pdf"
+	pdftk $forms/$name.pdf dump_data_fields > $forms/$name.fields
 else
-  names="`cat ops/sources/$source.json | jq '.data | map(.name)' | tr -d ' ,"[]' | tr '\n\r' ' '`"
+  names="`cat ops/sources.json | jq '.data | map(.name)' | tr -d ' ,"[]' | tr '\n\r' ' '`"
   for name in $names
   do
-    url="`cat ops/sources/$source.json | jq '.data | map(select(.name == "'$name'"))[0].url' | tr -d '"'`"
+    url="`cat ops/sources.json | jq '.data | map(select(.name == "'$name'"))[0].url' | tr -d '"'`"
 
     echo "curl -s $url > $forms/$name.pdf"
     curl -s $url > $forms/$name.pdf
