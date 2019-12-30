c7f2aaf2772c7d491e77c1653eb7e964d761998c
Bo Henderson <bohende@gmail.com>
2019-12-30 18:49:47 +0530

    ✨ init stubs for required form parsers

--- src/entry.js
+++ src/entry.js
@@ -5,6 +5,13 @@ const year = require('../package.json').year
 const { add, mul, diff } = require("./math");
 const { parseHistory } = require("./parse-history.js");
 
+const { parseF1040 } = require("./f1040");
+const { parseF1040s1 } = require("./f1040s1");
+const { parseF1040s2 } = require("./f1040s2");
+const { parseF1040s3 } = require("./f1040s3");
+const { parseF1040sc } = require("./f1040sc");
+const { parseF1040sd } = require("./f1040sd");
+const { parseF1040sse } = require("./f1040sse");
 const { parseF8949 } = require("./f8949");
 
 const personalData = require(`${process.cwd()}/${process.argv[2]}`)
@@ -12,15 +19,30 @@ const outputFolder = `${process.cwd()}/${process.argv[3]}`
 
 console.log('Lets go');
 
-const forms = {}
+const rawForms = {}
 
 const txHistory = parseHistory(personalData)
-const f8949Forms = parseF8949(personalData, txHistory);
 
-let i = 1
-for (const form of f8949Forms) {
-  forms[`f8949_${i}`] = form;
-  i += 1;
+rawForms.f8949 = parseF8949(personalData, txHistory);
+rawForms.f1040 = parseF1040(personalData);
+rawForms.f1040s1 = parseF1040s1(personalData);
+rawForms.f1040s2 = parseF1040s2(personalData);
+rawForms.f1040s3 = parseF1040s3(personalData);
+rawForms.f1040sc = parseF1040sc(personalData);
+rawForms.f1040sd = parseF1040sd(personalData);
+rawForms.f1040sse = parseF1040sse(personalData);
+
+const forms = {}
+for (const [name, data] of Object.entries(rawForms)) {
+  if (data.length === 1) {
+    forms[name] = data[0];
+  } else {
+    let i = 1
+    for (const page of data) {
+      forms[`f8949_${i}`] = page;
+      i += 1;
+    }
+  }
 }
 
 for (const [name, data] of Object.entries(forms)) {
---
+++ src/f1040sc.js
@@ -0,0 +1,15 @@
+const year = require('../package.json').year
+
+const f1040scMappings = require('./mappings/f1040sc.json');
+const { add, eq, gt, lt, mul, round, sub } = require('./math');
+const { emptyForm, mergeForms } = require('./utils');
+
+const debugMode = false;
+
+const parseF1040sc = (personal) => {
+  const f1040sc = mergeForms(emptyForm(f1040scMappings), personal.f1040sc || {});
+
+  return [f1040sc]
+}
+
+module.exports = { parseF1040sc }
