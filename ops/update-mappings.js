9518507f2475c460688ce1275bf2b5ada4db84d1
Bo Henderson <bohende@gmail.com>
2020-01-01 12:27:12 +0530

    🚚 mv update-test logic into update-mappings

--- ops/update-mappings.js
+++ ops/update-mappings.js
@@ -53,3 +53,31 @@ for (const form of test.forms) {
 
   fs.writeFileSync(mappingsFile(form), JSON.stringify(mappings, null, 2));
 }
+
+for (const form of test.forms) {
+  const mappings = JSON.parse(fs.readFileSync(mappingsFile(form), { encoding: 'utf8' }));
+  if (!mappings) {
+    throw new Error(`Mappings for form ${form} do not exist!`);
+  }
+
+  if (!test[form]) {
+    console.log(`Test values for form ${form} do not exist, adding...`);
+    test[form] = {}
+  }
+
+  for (const [key, value] of Object.entries(test[form])) {
+    if (!Object.keys(mappings).includes(key)) {
+      console.log(`Key ${key} of ${form} is in test but not mappings, removing..`);
+      delete test[form][key]
+    }
+  }
+
+  for (const [key, value] of Object.entries(mappings)) {
+    if (!Object.keys(test[form]).includes(key)) {
+      console.log(`Key ${key} of ${form} is in mappings but in test, adding..`);
+      test[form][key] = key
+    }
+  }
+}
+
+fs.writeFileSync(`test.json`, JSON.stringify(test, null, 2));
