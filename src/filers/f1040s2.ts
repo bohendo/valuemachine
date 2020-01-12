332dca9ba02c1517d709be86b07d61669a21b503
Bo Henderson <bohende@gmail.com>
2020-01-12 18:55:12 +0530

    🦅 finish f1040s2 filer

--- src/filers/f1040s2.ts
+++ src/filers/f1040s2.ts
@@ -1,7 +1,19 @@
 import { InputData, Forms } from '../types';
+import { add, round } from '../utils';
 
 export const f1040s2 = (input: InputData, oldForms: Forms): Forms => {
   const forms = JSON.parse(JSON.stringify(oldForms)) as Forms;
+  const f1040s2 = forms.f1040s2 && forms.f1040s2[0] ? forms.f1040s2[0] : {};
+
+  f1040s2.FullName = `${input.FirstName} ${input.MiddleInitial} ${input.LastName}`;
+  f1040s2.SSN = input.SocialSecurityNumber;
+
+  f1040s2.Line3 = round(add([f1040s2.Line1, f1040s2.Line2]));
+  f1040s2.Line10 = round(add([
+    f1040s2.Line4, f1040s2.Line5, f1040s2.Line6, f1040s2.Line7a, f1040s2.Line7b, f1040s2.Line8,
+  ]));
+
+  forms.f1040s2 = [f1040s2];
   return forms;
 }
 
--- src/filers/template.ts
+++ src/filers/template.ts
@@ -1,13 +1,13 @@
 import { InputData, Forms } from '../types';
 
-export const f1234 = (input: InputData, oldForms: Forms): Forms => {
+export const f1040s1 = (input: InputData, oldForms: Forms): Forms => {
   const forms = JSON.parse(JSON.stringify(oldForms)) as Forms;
-  const f1234 = forms.f1234 && forms.f1234[0] ? forms.f1234[0] : {};
+  const f1040s1 = forms.f1040s1 && forms.f1040s1[0] ? forms.f1040s1[0] : {};
 
-  f1234.FullName = `${input.FirstName} ${input.MiddleInitial} ${input.LastName}`;
-  f1234.SSN = input.SocialSecurityNumber;
+  f1040s1.FullName = `${input.FirstName} ${input.MiddleInitial} ${input.LastName}`;
+  f1040s1.SSN = input.SocialSecurityNumber;
 
-  forms.f1234 = [f1234];
+  forms.f1040s1 = [f1040s1];
   return forms;
 }
 
