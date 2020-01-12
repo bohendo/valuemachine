a94b0df94887970c60d22b713a2838b4b8789c12
Bo Henderson <bohende@gmail.com>
2020-01-12 18:14:39 +0530

    🐛 fix bug in f1040sse filer

--- src/filers/f1040sse.ts
+++ src/filers/f1040sse.ts
@@ -4,6 +4,7 @@ import { add, gt, lt, mul, round } from '../utils';
 export const f1040sse = (input: InputData, oldForms: Forms): Forms => {
   const forms = JSON.parse(JSON.stringify(oldForms)) as Forms;
   const f1040s1 = forms.f1040s1 && forms.f1040s1[0] ? forms.f1040s1[0] : {};
+  const f1040s2 = forms.f1040s2 && forms.f1040s2[0] ? forms.f1040s2[0] : {};
   const f1040sse = forms.f1040sse && forms.f1040sse[0] ? forms.f1040sse[0] : {};
 
   f1040sse.f1_1 = `${input.FirstName} ${input.MiddleInitial} ${input.LastName}`;
@@ -23,12 +24,13 @@ export const f1040sse = (input: InputData, oldForms: Forms): Forms => {
       ? add([mul(f1040sse.f1_7, "0.029"), "16479.60"])
       : mul(f1040sse.f1_7, "0.153")
   );
-  f1040s1.Line4 = f1040sse.f1_8
+  f1040s2.Line4 = f1040sse.f1_8
 
   f1040sse.f1_9 = round(mul(f1040sse.f1_8, "0.5"));
   f1040s1.Line14 = f1040sse.f1_9;
 
   forms.f1040s1 = [f1040s1];
+  forms.f1040s2 = [f1040s2];
   forms.f1040sse = [f1040sse];
   return forms;
 }
