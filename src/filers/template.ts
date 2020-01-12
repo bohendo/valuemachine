5daa36adc7162741d4d1d725b268a33fbf4d95ca
Bo Henderson <bohende@gmail.com>
2020-01-12 18:48:31 +0530

    ⚡ upgrade filer template

--- src/filers/template.ts
+++ src/filers/template.ts
@@ -2,6 +2,12 @@ import { InputData, Forms } from '../types';
 
 export const f1234 = (input: InputData, oldForms: Forms): Forms => {
   const forms = JSON.parse(JSON.stringify(oldForms)) as Forms;
+  const f1234 = forms.f1234 && forms.f1234[0] ? forms.f1234[0] : {};
+
+  f1234.FullName = `${input.FirstName} ${input.MiddleInitial} ${input.LastName}`;
+  f1234.SSN = input.SocialSecurityNumber;
+
+  forms.f1234 = [f1234];
   return forms;
 }
 
