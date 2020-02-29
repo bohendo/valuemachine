45b13553c751a3908b5b9376b635d84d5bb5575f
Bo Henderson <bohende@gmail.com>
2020-02-29 13:06:50 +0530

    ✨ init set/newContext methods on Logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,18 +5,26 @@ export class Logger {
     info: 3,
     warn: 2,
   };
-  private name = "Logger";
+  private context = "Logger";
 
   public logLevel = 3;
 
-  public constructor(name?: string, logLevel?: number) {
-    this.name = typeof name !== "undefined" ? name : this.name;
+  public constructor(context?: string, logLevel?: number) {
+    this.context = typeof context !== "undefined" ? context : this.context;
     this.logLevel =
       typeof logLevel !== "undefined" ? parseInt(logLevel.toString(), 10) : this.logLevel;
   }
 
   public setLevel(logLevel: number): void {
     this.logLevel = logLevel;
+  }
+
+  public setContext(context: string): void {
+    this.context = context;
+  }
+
+  public newContext(context: string, logLevel?: number): Logger {
+    return new Logger(context, logLevel || this.logLevel);
   }
 
   public error(msg: string): void {
@@ -37,6 +45,6 @@ export class Logger {
 
   private log(level: string, msg: any): void {
     if (this.levels[level] > this.logLevel) return;
-    return (console as any)[level](`${level.substring(0, 1).toUpperCase()} [${this.name}] ${msg}`);
+    return (console as any)[level](`${level.substring(0, 1).toUpperCase()} [${this.context}] ${msg}`);
   }
 }
