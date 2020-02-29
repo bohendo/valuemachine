58e4384461b755ad3aaf65473099eec674f32ec6
Bo Henderson <bohende@gmail.com>
2020-02-29 12:14:47 +0530

    ⚡️ getCapitalGains: track withdrawals/deposits to/from defi apps

--- src/getCapitalGains.ts
+++ src/getCapitalGains.ts
@@ -33,121 +33,196 @@ export const getCapitalGains = async (
   events: Event[],
 ): Promise<CapitalGain[]> => {
   const log = new Logger("CapitalGains", input.logLevel);
-  const assets: { [key: string]: Asset[] } = {};
-  const startingAssets: { [key: string]: Asset[] } = {};
+  const assets: { [app: string]: { [type: string]: Asset[] }} = { self: {} };
   const trades = [];
   const total = { cost: {}, proceeds: {}, profit: {} };
 
   log.info(`Parsing ${events.length} events for taxable trades..`);
 
   // TODO: what if input.capitalGainsMethod is LIFO or HIFO?
-  const getNext = (type: string): Asset => assets[type].pop();
-  const putBack = (type: string, asset: Asset): number => assets[type].unshift(asset);
+  const getNext = (type: string): Asset => assets.self[type] ? assets.self[type].pop() : undefined;
+  const putBack = (type: string, asset: Asset): number => assets.self[type].unshift(asset);
 
   for (const event of events.sort(
     (e1, e2) => new Date(e1.date).getTime() - new Date(e2.date).getTime(),
   )) {
     const date = event.date;
 
-    // if (event.description.includes("BAT")) { log.setLevel(5); } else { log.setLevel(0); }
+    // if (event.description.includes("ETH")) { log.setLevel(5); } else { log.setLevel(3); }
 
     log.info(`Processing event: ${event.description || JSON.stringify(event)}`);
-    log.debug(`Processing event: ${JSON.stringify(event)}`);
+    // log.debug(`Processing event: ${JSON.stringify(event)}`);
 
     for (const asset of event.assetsIn) {
-      if (["USD", "SAI", "DAI"].includes(asset.type)) {
-        continue; // Buying USD-equivalents doesn't need to be tracked
+      const app = event.category === "withdrawal" ? event.from : "self";
+      if (!assets[app]) {
+        log.debug(`Creating new asset app for ${app}`);
+        assets[app] = {};
       }
-      if (!assets[asset.type]) {
-        log.debug(`Creating new asset category for ${asset.type}`);
-        assets[asset.type] = [];
+      if (!assets[app][asset.type]) {
+        log.debug(`Creating new asset category for ${app}: ${asset.type}`);
+        assets[app][asset.type] = [];
       }
-      assets[asset.type].push({
-        amount: asset.amount,
-        date,
-        price: event.prices[asset.type] || await getPrice(input, asset.type, date),
-        type: asset.type,
-      });
+
+      if (app !== "self") {
+
+        // eslint-disable-next-line no-constant-condition
+        let toWithdraw = asset.amount;
+        while (gt(toWithdraw, "0")) {
+          const chunk = assets[app][asset.type].pop();
+          if (!chunk) {
+            // TODO: identify exchanges or calculate interest gained
+            if (!assets.self[asset.type]) {
+              assets.self[asset.type] = [];
+            }
+            assets.self[asset.type].push({
+              amount: toWithdraw,
+              date,
+              price: event.prices[asset.type] || await getPrice(input, asset.type, date),
+              type: asset.type,
+            });
+            log.warn(`Withdrew ${toWithdraw} more ${asset.type} from ${app} than we've deposited`);
+            toWithdraw = "0";
+          } else if (eq(chunk.amount, toWithdraw)) {
+            assets.self[asset.type].push(chunk);
+            toWithdraw = sub(toWithdraw, chunk.amount);
+            log.debug(`Withdrew chunk of ${chunk.amount} ${chunk.type} from ${app} (done)`);
+          } else if (gt(chunk.amount, toWithdraw)) {
+            const chunkLeft = sub(chunk.amount, toWithdraw);
+            assets.self[asset.type].push({
+              amount: sub(chunk.amount, chunkLeft),
+              ...chunk,
+            });
+            assets[app][asset.type].unshift({
+              amount: chunkLeft,
+              ...chunk,
+            });
+            toWithdraw = sub(toWithdraw, chunk.amount);
+            log.debug(`Withdrew partial chunk of ${chunk.amount} ${chunk.type} from ${app}`);
+          } else if (lt(chunk.amount, toWithdraw)) {
+            assets.self[asset.type].push(chunk);
+            toWithdraw = sub(toWithdraw, chunk.amount);
+            log.debug(`Withdrew chunk of ${chunk.amount} ${chunk.type} from ${app} (cont)`);
+          }
+        }
+
+      } else {
+
+        assets[app][asset.type].push({
+          amount: asset.amount,
+          date,
+          price: event.prices[asset.type] || await getPrice(input, asset.type, date),
+          type: asset.type,
+        });
+
+      }
     }
 
     for (const asset of event.assetsOut) {
-      if (["USD", "SAI", "DAI"].includes(asset.type)) {
-        continue; // Selling USD-equivalents never yields cap gains
+      const app = event.category === "deposit" ? event.to : "self";
+      if (!assets[app]) {
+        log.debug(`Creating new asset app for ${app}`);
+        assets[app] = {};
+      }
+      if (!assets[app][asset.type]) {
+        log.debug(`Creating new asset category for ${app}: ${asset.type}`);
+        assets[app][asset.type] = [];
       }
 
-      if (!assets[asset.type]) {
-        log.debug(`Creating new asset category for ${asset.type}`);
-        assets[asset.type] = [];
+      // Deposit this asset into defi app
+      if (app !== "self") {
+
+        // eslint-disable-next-line no-constant-condition
+        let toDeposit = asset.amount;
+        while (gt(toDeposit, "0")) {
+          const chunk = getNext(asset.type);
+          if (eq(chunk.amount, toDeposit)) {
+            assets[app][asset.type].push(chunk);
+            toDeposit = sub(toDeposit, chunk.amount);
+            log.debug(`Deposited chunk of ${chunk.amount} ${chunk.type} into ${app} (done)`);
+          } else if (gt(chunk.amount, toDeposit)) {
+            const chunkLeft = sub(chunk.amount, toDeposit);
+            assets[app][asset.type].push({
+              amount: sub(chunk.amount, chunkLeft),
+              ...chunk,
+            });
+            toDeposit = sub(toDeposit, chunk.amount);
+            assets.self[asset.type].unshift({
+              amount: chunkLeft,
+              ...chunk,
+            });
+            log.debug(`Deposited partial chunk of ${chunk.amount} ${chunk.type} into ${app}`);
+          } else if (lt(chunk.amount, toDeposit)) {
+            assets[app][asset.type].push(chunk);
+            toDeposit = sub(toDeposit, chunk.amount);
+            log.debug(`Deposited chunk of ${chunk.amount} ${chunk.type} into ${app} (cont)`);
+          }
+        }
+
+      } else {
+
+        let amt = asset.amount;
+        let cost = "0";
+        let profit = "0";
+        let proceeds = "0";
+
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          if (eq(amt, "0") || lt(amt, "0")) {
+            break;
+          }
+
+          const chunk = getNext(asset.type);
+          if (!chunk) {
+            throw new Error(`Selling more ${asset.type} than we have: ${JSON.stringify(assets, null, 2)}`);
+            break;
+          }
+
+          const price = event.prices[asset.type] || await getPrice(input, asset.type, date);
+          if (!price) {
+            throw new Error(`Chunk is missing a price: ${JSON.stringify(chunk, null, 2)}`);
+          }
+
+          if (eq(chunk.amount, amt)) {
+            profit = add([profit, mul(amt, sub(price, chunk.price))]);
+            cost = add([cost, mul(chunk.price, amt)]);
+            chunk.amount = sub(chunk.amount, amt);
+            log.debug(`Sold chunk of ${chunk.amount} ${chunk.type} for profit of ${profit} (done)`);
+            break;
+          } else if (gt(chunk.amount, amt)) {
+            profit = add([profit, mul(amt, sub(price, chunk.price))]);
+            cost = add([cost, mul(chunk.price, amt)]);
+            chunk.amount = sub(chunk.amount, amt);
+            putBack(asset.type, chunk);
+            log.debug(`Sold partial chunk of ${chunk.amount} ${chunk.type} for profit of ${profit}`);
+            break;
+          } else {
+            profit = add([profit, mul(chunk.amount, sub(price, chunk.price))]);
+            cost = add([cost, mul(chunk.price, chunk.amount)]);
+            amt = sub(amt, chunk.amount);
+            log.debug(`Sold chunk of ${chunk.amount} ${chunk.type} for profit of ${profit} (cont)`);
+          }
+        }
+        proceeds = mul(asset.price, asset.amount);
+
+        trades.push({
+          Adjustment: "",
+          Code: "",
+          Cost: cost,
+          DateAcquired: "VARIOUS",
+          // DateSold: month/day/year
+          DateSold: `${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(2,4)}`,
+          Description: `${round(asset.amount)} ${asset.type}`,
+          GainOrLoss: profit,
+          Proceeds: proceeds,
+        });
+
+        total.proceeds[asset.type] = add([total.proceeds[asset.type], proceeds]);
+        total.cost[asset.type] = add([total.cost[asset.type], cost]);
+        total.profit[asset.type] = add([total.profit[asset.type], profit]);
+
       }
-
-      let amt = asset.amount;
-      let cost = "0";
-      let profit = "0";
-      let proceeds = "0";
-
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        if (eq(amt, "0") || lt(amt, "0")) {
-          break;
-        }
-
-        const chunk = getNext(asset.type);
-        if (!chunk) {
-          throw new Error(`Attempting to sell more ${asset.type} than we bought.`);
-          break;
-        }
-
-        const price = event.prices[asset.type] || await getPrice(input, asset.type, date);
-        if (!price) {
-          throw new Error(`Chunk is missing a price; ${JSON.stringify(chunk, null, 2)}`);
-        }
-
-        if (eq(chunk.amount, amt)) {
-          profit = add([profit, mul(amt, sub(price, chunk.price))]);
-          cost = add([cost, mul(chunk.price, amt)]);
-          chunk.amount = sub(chunk.amount, amt);
-          log.debug(`Selling exact chunk of ${asset.type} for profit of ${profit} to finish sale`);
-          break;
-        } else if (gt(chunk.amount, amt)) {
-          profit = add([profit, mul(amt, sub(price, chunk.price))]);
-          cost = add([cost, mul(chunk.price, amt)]);
-          chunk.amount = sub(chunk.amount, amt);
-          putBack(asset.type, chunk);
-          log.debug(`Selling part of chunk of ${asset.type} for profit of ${profit} to finish sale`);
-          break;
-        } else {
-          profit = add([profit, mul(chunk.amount, sub(price, chunk.price))]);
-          cost = add([cost, mul(chunk.price, chunk.amount)]);
-          amt = sub(amt, chunk.amount);
-          log.debug(`Selling entire chunk of ${asset.type} for profit of ${profit} and continuing sale`);
-        }
-      }
-      proceeds = mul(asset.price, asset.amount);
-
-      trades.push({
-        Adjustment: "",
-        Code: "",
-        Cost: cost,
-        DateAcquired: "VARIOUS",
-        // DateSold: month/day/year
-        DateSold: `${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(2,4)}`,
-        Description: `${round(asset.amount)} ${asset.type}`,
-        GainOrLoss: profit,
-        Proceeds: proceeds,
-      });
-
-      total.proceeds[asset.type] = add([total.proceeds[asset.type], proceeds]);
-      total.cost[asset.type] = add([total.cost[asset.type], cost]);
-      total.profit[asset.type] = add([total.profit[asset.type], profit]);
-
     }
-
-    for (const asset of Object.keys(assets)) {
-      if (total.cost[asset] && total.proceeds[asset] && total.profit[asset]) {
-        log.debug(`So far, we've spent $${round(total.cost[asset])}, recieved $${round(total.proceeds[asset])}, and profited $${round(total.profit[asset])} from ${asset}`);
-      }
-    }
-
   }
 
   ////////////////////////////////////////
@@ -160,12 +235,11 @@ export const getCapitalGains = async (
   const totalProfit =
     Object.keys(total.profit).reduce((cur, acc) => add([acc, total.profit[cur]]), "0");
 
-  log.info(`Starting Assets: ${startingAssets}`); 
   for (const trade of trades) {
     log.info(`Sold ${trade.Description} on ${trade.DateSold} for ${trade.Proceeds} (Purchased for ${trade.Cost} = profit of ${trade.GainOrLoss}`);
   }
-  log.info(`Assets Leftover: ${stringifyAssets(assets)}`); 
-  log.info(`Total proceeds: ${totalProceeds} - cost ${totalCost} = profit ${totalProfit}`); 
+  log.info(`Assets Leftover: ${stringifyAssets(assets.self)}`);
+  log.info(`Total proceeds: ${totalProceeds} - cost ${totalCost} = profit ${totalProfit}`);
 
   return trades;
 };
