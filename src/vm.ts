01da9ecf68b83b93c3d8a739e586972306585b85
Bo Henderson <bohende@gmail.com>
2020-03-02 17:26:46 +0530

    📛 add checkpoint assertions to vm

--- src/vm.ts
+++ src/vm.ts
@@ -10,17 +10,69 @@ import {
 } from "./types";
 import { add, eq, gt, Logger, sub } from "./utils";
 
+const checkpoints = [
+  {
+    account: "0xada083a3c06ee526f827b43695f2dcff5c8c892b",
+    assetType: "ETH",
+    balance: "0",
+    date: "2017-11-21T07:54:38.000Z",
+  },
+  {
+    account: "0xada083a3c06ee526f827b43695f2dcff5c8c892b",
+    assetType: "ETH",
+    balance: "6",
+    date: "2017-12-05T23:14:55.000Z",
+  },
+  {
+    account: "0xada083a3c06ee526f827b43695f2dcff5c8c892b",
+    assetType: "ETH",
+    balance: "1.472343111572222222",
+    date: "2017-12-30T15:14:53.000Z",
+  },
+  {
+    account: "0xada083a3c06ee526f827b43695f2dcff5c8c892b",
+    assetType: "ETH",
+    balance: "1.102702839572222222",
+    date: "2018-02-16T04:08:45.000Z",
+  },
+];
+
+const assertState = (state: State, event: Event): void => {
+  for (const { account, assetType, balance, date } of checkpoints) {
+    if (date === event.date) {
+      let actual;
+      if (!state[account] || !state[account][assetType]) {
+        if (!eq(balance, "0")) {
+          throw new Error(`Expected accout ${account} to have ${assetType} balance of ${balance} on ${date} but got 0`);
+        }
+        actual = "0";
+      } else {
+        actual = state[account][assetType]
+          .reduce((sum, chunk) => add([sum, chunk.quantity]), "0");
+      }
+      if (!eq(actual, balance)) {
+        throw new Error(`Expected accout ${account} to have ${assetType} balance of ${balance} on ${date} but got ${actual}`);
+      }
+    }
+  }
+};
+
 export const getValueMachine = (addressBook: AddressBook): any => {
   const log = new Logger("ValueMachine", env.logLevel);
   const { isSelf, pretty } = addressBook;
 
+  const offTheChain = (assetType: AssetType): boolean =>
+    ["BTC", "INR", "LTC", "USD"].includes(assetType);
+
   const noValueError = (account: string, assetType: string): string =>
-    `${account} attempted to spend more ${assetType} than they had.`;
+    `${account} attempted to spend more ${assetType} than they received.`;
 
   // TODO: what if input.capitalGainsMethod is LIFO or HIFO?
   const getPutChunk = (state: State) =>
     (account: string, assetType: string, asset: AssetChunk): void => {
-      if (assetType === "USD" || !isSelf(account)) {
+      log.debug(`Putting ${asset.quantity} ${assetType} into account ${account}`);
+      if (offTheChain(assetType) || !isSelf(account)) {
+        log.debug(`Skipping off-chain or external asset put`);
         return;
       }
       if (!state[account]) {
@@ -76,28 +128,82 @@ export const getValueMachine = (addressBook: AddressBook): any => {
       return output;
     };
 
+  const getGetBalance = (state: State) =>
+    (account: string, assetType: AssetType): DecimalString =>
+      !state[account]
+        ? "0"
+        : !state[account][assetType]
+        ? "0"
+        : !state[account][assetType].length
+        ? "0"
+        : state[account][assetType].reduce((sum, chunk) => add([sum, chunk.quantity]), "0");
+
   return (oldState: State | null, event: Event): [State, Log] => {
     log.info(`${event.date} ${event.description}`);
     const state = JSON.parse(JSON.stringify(oldState || {})) as State;
     log.debug(`Applying event ${JSON.stringify(event, null, 2)} to state ${JSON.stringify(state, null, 2)}`);
     const logs = [];
-    const [getChunks, putChunk] = [getGetChunk(state, event), getPutChunk(state)];
+    const [getChunks, putChunk, getBalance] =
+      [getGetChunk(state, event), getPutChunk(state), getGetBalance(state)];
+
+    assertState(state, event);
 
     for (const { assetType, from, quantity, to } of event.transfers) {
       log.info(`transfering ${quantity} ${assetType} from ${pretty(from)} to ${pretty(to)}`);
 
+      let startingBalance = getBalance(from, assetType);
+      if (isSelf(from) && isSelf(to)) {
+        log.debug(`Starting ${assetType} balances | sender ${startingBalance} | recipient ${getBalance(to, assetType)}`);
+      } else if (isSelf(from)) {
+        log.debug(`Starting ${assetType} balances | sender ${startingBalance} | recipient external`);
+      } else if (isSelf(to)) {
+        log.debug(`Starting ${assetType} balances | sender external | recipient ${getBalance(to, assetType)}`);
+      } else {
+        log.debug(`Skipping external-to-external transfer`);
+        continue;
+      }
+
+
       const chunks = getChunks(from, assetType, quantity);
+      let endingBalance = getBalance(from, assetType);
+      let balanceDiff = sub(startingBalance, endingBalance);
 
       log.debug(`got chunks ${JSON.stringify(chunks)}`);
 
       const total = chunks.reduce((sum, chunk) => add([sum, chunk.quantity]), "0");
 
       if (!eq(total, quantity)) {
-        throw new Error(`getChunk got ${total} but expected ${quantity}`);
+        throw new Error(`getChunk got total of ${total} ${assetType} but expected ${quantity}`);
       }
 
+      if (!offTheChain(assetType) && isSelf(from) && !eq(quantity, balanceDiff)) {
+        throw new Error(`Expected sender ${assetType} balance to decrease by ${quantity} but got ${balanceDiff}`);
+      }
+
+      startingBalance = getBalance(to, assetType);
       chunks.forEach(chunk => putChunk(to, assetType, chunk));
+      endingBalance = getBalance(to, assetType);
+      balanceDiff = sub(endingBalance, startingBalance);
+
+      if (!offTheChain(assetType) && isSelf(to) && !eq(quantity, balanceDiff)) {
+        throw new Error(`Expected recipient ${assetType} balance to increase by ${quantity} but got ${balanceDiff}`);
+      }
+
+      if (isSelf(from) && isSelf(to)) {
+        log.debug(`Ending ${assetType} balances | sender ${getBalance(from, assetType)} | recipient ${getBalance(to, assetType)}`);
+      } else if (isSelf(from)) {
+        log.debug(`Ending ${assetType} balances | sender ${getBalance(from, assetType)} | recipient external`);
+      } else if (isSelf(to)) {
+        log.debug(`Ending ${assetType} balances | sender external | recipient ${getBalance(to, assetType)}`);
+      } else {
+        throw new Error(`We shouldn't have actually processed an external-to-external transfer`);
+        continue;
+      }
+
     }
+    log.debug(`Final state after applying ${event.date} ${event.description}: ${
+      JSON.stringify(state, null, 2)
+    }`);
 
     return [state, logs];
   };
