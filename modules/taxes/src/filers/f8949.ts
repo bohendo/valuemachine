1746822142aa345e26cc394327fe756d235ed8bc
bohendo <bohende@gmail.com>
2020-04-30 18:09:22 +0530

    💎 polish f8949 filer

--- modules/taxes/src/filers/f8949.ts
+++ modules/taxes/src/filers/f8949.ts
@@ -2,7 +2,7 @@ import { ContextLogger, LevelLogger } from "@finances/core";
 import { CapitalGainsLog, Logs, LogTypes } from "@finances/types";
 
 import { env } from "../env";
-import { add, div, eq, gt, mul, round, sub, toFormDate } from "../utils";
+import { add, eq, gt, mul, round, sub, toFormDate } from "../utils";
 import { Forms } from "../types";
 
 const msPerDay = 1000 * 60 * 60 * 24;
@@ -17,100 +17,71 @@ export const f8949 = (vmLogs: Logs, oldForms: Forms): Forms  => {
   ////////////////////////////////////////
   // Format results into forms
 
+  const getDate = (timestamp: string): string => timestamp.split("T")[0];
+
   // Merge trades w the same recieved & sold dates
-  const mergedTrades = [];
+  const trades = [];
   vmLogs
     .filter(vmLog => vmLog.type === LogTypes.CapitalGains)
     .filter(vmLog => !eq(round(vmLog.quantity, 4), "0"))
+    .filter((trade: CapitalGainsLog) => getDate(trade.date) !== getDate(trade.purchaseDate))
     .forEach((trade: CapitalGainsLog): void => {
-      const dup = mergedTrades.findIndex(merged =>
+      const dup = trades.findIndex(merged =>
         merged.assetType === trade.assetType &&
-        merged.date.split("T")[0] === trade.date.split("T")[0] &&
-        merged.purchaseDate.split("T")[0] === trade.purchaseDate.split("T")[0],
+        getDate(merged.date) === getDate(trade.date) &&
+        getDate(merged.purchaseDate) === getDate(trade.purchaseDate),
       );
       if (dup !== -1) {
-        mergedTrades[dup].quantity = add([mergedTrades[dup].quantity, trade.quantity]);
+        trades[dup].quantity = add([trades[dup].quantity, trade.quantity]);
       } else {
-        mergedTrades.push(trade);
+        trades.push(trade);
       }
     });
 
-  // Sort trades into long-term/short-term
-  const shortTermTrades = mergedTrades.filter(trade =>
-    (new Date(trade.date).getTime() - new Date(trade.purchaseDate).getTime()) < msPerYear,
-  );
-
-  const longTermTrades = mergedTrades.filter(trade =>
-    (new Date(trade.date).getTime() - new Date(trade.purchaseDate).getTime()) >= msPerYear,
-  );
-
-  // Build a series of forms from chunks of trades
-  const chunkSize = 14;
-
-  const longChunks = longTermTrades.map((e,i) =>
-     i % chunkSize === 0 ? longTermTrades.slice(i, i + chunkSize) : null,
-  ).filter(e => !!e);
-
-  const shortChunks = shortTermTrades.map((e,i) =>
-     i % chunkSize === 0 ? shortTermTrades.slice(i, i + chunkSize) : null,
-  ).filter(e => !!e);
-
-  const nPages = longChunks.length > shortChunks.length ? longChunks.length : shortChunks.length;
+  // Sort trades into chunks of 14 long-term/short-term trades
 
   const columns = ["d", "e", "g", "h"];
   const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  const isLongTerm = (trade: CapitalGainsLog): boolean => 
+    (new Date(trade.date).getTime() - new Date(trade.purchaseDate).getTime()) >= msPerYear;
+
+  const chunkify = (list: any[]): any[][] => list.map(
+    (e,i) => i % rows.length === 0 ? list.slice(i, i + rows.length) : null,
+  ).filter(e => !!e);
+
+  const shortChunks = chunkify(trades.filter(trade => !isLongTerm(trade)));
+  const longChunks = chunkify(trades.filter(isLongTerm));
+
+  const nPages = longChunks.length > shortChunks.length ? longChunks.length : shortChunks.length;
 
   const getLongCell = (row: number, column: string): string => `P2L1R${row}${column}`;
   const getShortCell = (row: number, column: string): string => `P1L1R${row}${column}`;
 
   // Convert chunk of vmLogs into f8949 rows
   for (let page = 0; page < nPages; page++) {
-    f8949.push(((): any => {
-      const shortTerm = shortChunks[page] || [];
-      const longTerm = longChunks[page] || [];
-      const subF8949 = {} as any;
-      subF8949.P1C0_C = shortTerm.length > 0;
-      let i = 1;
-      for (const trade of shortTerm) {
-        const description = `${round(trade.quantity, 4)} ${trade.assetType}`;
-        const proceeds = mul(trade.quantity, trade.assetPrice);
-        const cost = mul(trade.quantity, trade.purchasePrice);
-        const gainOrLoss = sub(proceeds, cost);
-        const timeHeld = round(div(
-          (new Date(trade.date).getTime() - new Date(trade.purchaseDate).getTime()).toString(),
-          msPerDay.toString(),
-        ));
-        log.info(`Sold ${description} after ${timeHeld} days | ${round(proceeds)} - ${round(cost)} = ${round(gainOrLoss)}`);
-        subF8949[getShortCell(i, "a")] = description;
-        subF8949[getShortCell(i, "b")] = toFormDate(trade.purchaseDate);
-        subF8949[getShortCell(i, "c")] = toFormDate(trade.date);
-        subF8949[getShortCell(i, "d")] = round(proceeds);
-        subF8949[getShortCell(i, "e")] = round(cost);
-        subF8949[getShortCell(i, "h")] = round(gainOrLoss);
-        i += 1;
-      }
-      subF8949.P2C0_F = longTerm.length > 0;
-      i = 1;
-      for (const trade of longTerm) {
-        const description = `${round(trade.quantity, 4)} ${trade.assetType}`;
-        const proceeds = mul(trade.quantity, trade.assetPrice);
-        const cost = mul(trade.quantity, trade.purchasePrice);
-        const gainOrLoss = sub(proceeds, cost);
-        const timeHeld = round(div(
-          (new Date(trade.date).getTime() - new Date(trade.purchaseDate).getTime()).toString(),
-          msPerDay.toString(),
-        ));
-        log.info(`Sold ${description} after ${timeHeld} days | ${round(proceeds)} - ${round(cost)} = ${round(gainOrLoss)}`);
-        subF8949[getLongCell(i, "a")] = description;
-        subF8949[getLongCell(i, "b")] = toFormDate(trade.purchaseDate);
-        subF8949[getLongCell(i, "c")] = toFormDate(trade.date);
-        subF8949[getLongCell(i, "d")] = round(proceeds);
-        subF8949[getLongCell(i, "e")] = round(cost);
-        subF8949[getLongCell(i, "h")] = round(gainOrLoss);
-        i += 1;
-      }
-      return subF8949;
-    })());
+    const shortTerm = shortChunks[page] || [];
+    const longTerm = longChunks[page] || [];
+    const subF8949 = {} as any;
+    subF8949.P1C0_C = shortTerm.length > 0;
+    subF8949.P2C0_F = longTerm.length > 0;
+    const parseTrade = getCell => (trade: CapitalGainsLog, index: number): void => {
+      const i = index + 1;
+      const description = `${round(trade.quantity, 4)} ${trade.assetType}`;
+      const proceeds = round(mul(trade.quantity, trade.assetPrice));
+      const cost = round(mul(trade.quantity, trade.purchasePrice));
+      const gainOrLoss = sub(proceeds, cost);
+      log.info(`Sold ${description} on ${trade.date}: ${proceeds} - ${cost} = ${gainOrLoss}`);
+      subF8949[getCell(i, "a")] = description;
+      subF8949[getCell(i, "b")] = toFormDate(trade.purchaseDate);
+      subF8949[getCell(i, "c")] = toFormDate(trade.date);
+      subF8949[getCell(i, "d")] = proceeds;
+      subF8949[getCell(i, "e")] = cost;
+      subF8949[getCell(i, "h")] = gainOrLoss;
+    };
+    shortTerm.forEach(parseTrade(getShortCell));
+    longTerm.forEach(parseTrade(getShortCell));
+    f8949.push(subF8949);
   }
 
   // Calculate totals from f8949 rows
@@ -135,8 +106,8 @@ export const f8949 = (vmLogs: Logs, oldForms: Forms): Forms  => {
         if (gt(page[longCell], "0")) {
           log.info(`Adding long ${page[longCell]} to ${longTotal[column]}`);
         }
-        shortTotal[column] = round(add([shortTotal[column], page[shortCell]]));
-        longTotal[column] = round(add([longTotal[column], page[longCell]]));
+        shortTotal[column] = add([shortTotal[column], page[shortCell]]);
+        longTotal[column] = add([longTotal[column], page[longCell]]);
       }
     }
     for (const column of columns) {
