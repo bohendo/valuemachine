d52dc47c601cd595df6b01f3d7d7c7425c91c86e
bohendo <bohende@gmail.com>
2020-05-13 10:44:10 +0530

    🦅 finish implementing income tax util

--- modules/taxes/src/utils.ts
+++ modules/taxes/src/utils.ts
@@ -1,9 +1,11 @@
 import { TimestampString } from "@finances/types";
 import { ContextLogger, LevelLogger, math } from "@finances/utils";
+import { MaxUint256 } from "ethers/constants";
 
 import { env } from "./env";
 
 export const getIncomeTax = (taxableIncome: string, filingStatus: string): string => {
+  const inf = MaxUint256.toString();
   const taxBrackets19 = [
     { rate: "0.10", single: "9700",   joint: "19400",  head: "13850" },
     { rate: "0.12", single: "39475",  joint: "78950",  head: "52850" },
@@ -11,24 +13,33 @@ export const getIncomeTax = (taxableIncome: string, filingStatus: string): strin
     { rate: "0.24", single: "160725", joint: "321450", head: "160700" },
     { rate: "0.32", single: "204100", joint: "408200", head: "204100" },
     { rate: "0.35", single: "510300", joint: "612350", head: "510300" },
-    { rate: "0.37", single: "510300", joint: "612350", head: "0" },
+    { rate: "0.37", single: inf, joint: inf, head: inf },
   ];
 
   let incomeTax = "0";
-  taxBrackets19.forEach((bracket, index) => {
-    if (math.gt(taxableIncome, bracket[filingStatus])) {
+  let prevThreshold = "0";
+  taxBrackets19.forEach(bracket => {
+    const threshold = bracket[filingStatus];
+    if (math.lt(taxableIncome, prevThreshold)) {
+      return;
+    } else if (math.lt(taxableIncome, threshold)) {
       incomeTax = math.add(
         incomeTax,
         math.mul(
-          bracket.rate, math.sub(
-            index === taxBrackets19.length - 1
-              ? taxBrackets19[index + 1][filingStatus]
-              : taxableIncome,
-            bracket[filingStatus],
-          ),
+          bracket.rate,
+          math.sub(taxableIncome, prevThreshold),
+        ),
+      );
+    } else {
+      incomeTax = math.add(
+        incomeTax,
+        math.mul(
+          bracket.rate,
+          math.sub(threshold, prevThreshold),
         ),
       );
     }
+    prevThreshold = threshold;
   });
   return incomeTax;
 };
