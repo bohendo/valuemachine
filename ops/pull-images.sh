7b564cd14341756a80981710308ce902232ce006
bohendo <bohende@gmail.com>
2020-08-02 17:23:57 +0530

    🐛 fix typo

--- ops/pull-images.sh
+++ ops/pull-images.sh
@@ -5,7 +5,7 @@ root="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 project="`cat $root/package.json | grep '"name":' | head -n 1 | cut -d '"' -f 4`"
 registryRoot="`cat $root/package.json | grep '"registry":' | head -n 1 | cut -d '"' -f 4`"
 organization="${CI_PROJECT_NAMESPACE:-`whoami`}"
-commit="`git rev-parse HEAD | head -c 8)`"
+commit="`git rev-parse HEAD | head -c 8`"
 
 registry="$registryRoot/$organization/$project"
 
