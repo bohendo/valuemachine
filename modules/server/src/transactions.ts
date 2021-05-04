ff6f79ad5a6891b40e9a100a33954853305663ac
bohendo <bohendo@pm.me>
2021-05-04 11:05:31 +0530

    server shouldn't save user's address book in store

--- modules/server/src/transactions.ts
+++ modules/server/src/transactions.ts
@@ -1,11 +1,9 @@
 import { getAddressBook, getTransactions } from "@finances/core";
-import { StoreKeys } from "@finances/types";
 import { getLogger } from "@finances/utils";
 import express from "express";
 
 import { chainData } from "./chaindata";
 import { env } from "./env";
-import { getStore } from "./store";
 import { getLogAndSend, STATUS_YOUR_BAD, STATUS_MY_BAD } from "./utils";
 
 const log = getLogger(env.logLevel).child({ module: "Transactions" });
@@ -14,17 +12,15 @@ export const transactionsRouter = express.Router();
 
 transactionsRouter.post("/", async (req, res) => {
   const logAndSend = getLogAndSend(res);
-  const username = req.username;
   const addressBookJson = req.body.addressBook;
   if (!addressBookJson || !addressBookJson.length) {
     return logAndSend(`A valid address book must be provided via POST body`, STATUS_YOUR_BAD);
   }
   const addressBook = getAddressBook(addressBookJson, log);
   const selfAddresses = addressBook.addresses.filter(a => addressBook.isSelf(a));
-  log.info(`Found ${selfAddresses.length} self addreses for user ${username}`);
-  const userStore = getStore(username);
+  log.info(`Syncing txns for ${selfAddresses.length} self addreses`);
   try {
-    const transactions = getTransactions({ addressBook, logger: log, store: userStore });
+    const transactions = getTransactions({ addressBook, logger: log });
     await transactions.mergeChainData(
       chainData.getAddressHistory(...selfAddresses),
     );
@@ -34,12 +30,4 @@ transactionsRouter.post("/", async (req, res) => {
     log.warn(e);
     logAndSend("Error syncing transactions", STATUS_MY_BAD);
   }
-});
-
-transactionsRouter.delete("/", async (req, res) => {
-  const logAndSend = getLogAndSend(res);
-  const username = req.username;
-  const userStore = getStore(username);
-  userStore.save(StoreKeys.Transactions, []);
-  logAndSend(`Cleared transaction data for ${username}`);
 });
