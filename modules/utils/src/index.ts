17b7b24c451bbb623b4e649ec8ccd92a467cc2a3
bohendo <bohendo@pm.me>
2021-06-16 14:09:42 +0530

    add fs & local stores to utils

--- modules/utils/src/index.ts
+++ modules/utils/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./eth";
 export * from "./logger";
 export * from "./math";
+export * from "./store";
 export * from "./strings";
 export * from "./transaction";
 export * from "./verify";
