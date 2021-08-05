dbbb87f7924c73f7a8ba658ed9199e4863d5040c
bohendo <bohendo@pm.me>
2021-08-05 13:42:38 +0530

    use latest version of webpack & ts-loader in server

--- ops/version-check.sh
+++ ops/version-check.sh
@@ -3,7 +3,7 @@ set -e
 
 root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)
 
-do_not_upgrade='webpack webpack-cli ts-loader'
+do_not_upgrade=''
 
 if [[ -n "$do_not_upgrade" ]]
 then
