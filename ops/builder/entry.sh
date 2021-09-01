152766dd3c99258bb95c0885eef02d5ee2fb5ab2
bohendo <bohendo@pm.me>
2021-09-01 12:41:41 +0530

    upgrade deps

--- ops/builder/entry.sh
+++ ops/builder/entry.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -e
 
-this_user="`id -u`:`id -g`"
+this_user="$(id -u):$(id -g)"
 user="$1"
 shift;
-cmd="$@"
+cmd="$*"
 
 finish() {
   if [[ "$this_user" != "$user" ]]
-  then
-    chown -R ${user} /root
-    #echo "Fixing permissions for $user"
-  #else echo "Same user, skipping permission fix"
+  then echo "Fixing permissions for $user" && chown -R "$user" /root
+  else echo "Same user, skipping permission fix"
   fi
 }
 trap finish EXIT SIGINT
 
-#echo "Running command as "$this_user" (target user: $user)"
+echo "Running command as $this_user (target user: $user)"
 bash -c "$cmd"
