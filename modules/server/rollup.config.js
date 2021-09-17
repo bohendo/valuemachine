d9cb4717a0d1ab6495d9697d869478a5a0e8d8ef
bohendo <bohendo@pm.me>
2021-09-17 10:54:27 -0400

    hacky fix for cicular dependencies inside readable-stream

--- modules/server/rollup.config.js
+++ modules/server/rollup.config.js
@@ -1,9 +1,13 @@
 import CommonJs from "@rollup/plugin-commonjs";
+import Replace from "@rollup/plugin-replace";
 import Json from "@rollup/plugin-json";
 import NodeResolve from "@rollup/plugin-node-resolve";
 import Typescript from "@rollup/plugin-typescript";
 
 import pkg from "./package.json";
+
+// NOTE: this config includes a hacky fix to handle the circular dependencies of readable-stream
+// for more info see: https://github.com/rollup/rollup/issues/1507#issuecomment-340550539
 
 export default {
   input: "./src/entry.ts",
@@ -13,18 +17,25 @@ export default {
   }],
   onwarn: (warning, warn) => {
     // Ignore known warnings
-    const fromPkg = (pkgName) =>
-      warning.id?.startsWith(`/root/node_modules/${pkgName}`);
-      // || warning.cycle?.every(elem => elem.includes(`node_modules/${pkgName}`));
+    const fromPkg = (pkgName) => warning.id?.startsWith(`/root/node_modules/${pkgName}`);
     if (warning.code === "THIS_IS_UNDEFINED" && fromPkg("@ethersproject")) return;
     if (warning.code === "EVAL" && fromPkg("depd")) return;
-    // if (warning.code === "CIRCULAR_DEPENDENCY" && fromPkg("readable-stream")) return;
     warn(warning);
   },
+  external: ["readable-stream", "readable-stream/transform"],
   plugins: [
     NodeResolve({
       exportConditions: ["node"],
       preferBuiltins: true,
+    }),
+    Replace({
+      delimiters: ["", ""],
+      preventAssignment: true,
+      values: {
+        "require('readable-stream/transform')": "require('stream').Transform",
+        'require("readable-stream/transform")': 'require("stream").Transform',
+        "readable-stream": "stream"
+      }
     }),
     Typescript({
       noEmitOnError: true,
@@ -33,6 +44,9 @@ export default {
       tsconfig: "./tsconfig.json"
     }),
     Json({ compact: true }),
-    CommonJs({ include: ["./src/entry.ts", /node_modules/] }),
+    CommonJs({
+      include: ["./src/entry.ts", /node_modules/],
+      transformMixedEsModules: true,
+    }),
   ],
 };
