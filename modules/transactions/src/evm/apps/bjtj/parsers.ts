d3c909bfce9c68d21600519044c03c817a5373fb
bohendo <bohendo@pm.me>
2021-09-22 18:25:01 -0400

    rm unnecessary bjtj parsing logic

--- modules/transactions/src/evm/apps/bjtj/parsers.ts
+++ modules/transactions/src/evm/apps/bjtj/parsers.ts
@@ -1,4 +1,3 @@
-import { formatEther } from "@ethersproject/units";
 import {
   AddressBook,
   EvmMetadata,
@@ -9,33 +8,19 @@ import {
 } from "@valuemachine/types";
 // import { insertVenue } from "@valuemachine/utils";
 
-import { parseEvent } from "../utils";
-
 import { bjtjV1Address, bjtjV2Address } from "./addresses";
 import { apps } from "./enums";
 
 export const appName = apps.BJTJ;
-
-const blackjackV1Abi = [
-  "function cashout(address winner, uint256 amount)",
-  "event Deposit(address indexed from, uint256 value)"
-];
-
-const blackjackV2Abi = [
-  "function cashout(address winner, uint256 amount)",
-  "event Deposit(address indexed dealer, address indexed from, uint256 value)",
-  "event Cashout(address indexed dealer, address indexed to, uint256 value)",
-  "event Overflow(address indexed dealer, uint256 value)"
-];
 
 export const coreParser = (
   tx: Transaction,
   evmTx: EvmTransaction,
   evmMeta: EvmMetadata,
   addressBook: AddressBook,
-  logger: Logger,
+  _logger: Logger,
 ): Transaction => {
-  const log = logger.child({ module: `${appName}:${evmTx.hash.substring(0, 6)}` });
+  // const log = logger.child({ module: `${appName}:${evmTx.hash.substring(0, 6)}` });
   const appAccount = `${evmMeta.name}/${appName}`;
 
   // set transfer categories based on to/from balues
@@ -56,62 +41,6 @@ export const coreParser = (
       transfer.to = appAccount;
     }
   });
-
-  for (const txLog of evmTx.logs) {
-    const address = txLog.address;
-
-    if (address === bjtjV1Address) {
-      const event = parseEvent(blackjackV1Abi, txLog, evmMeta);
-      if (!event.name) continue;
-      tx.apps.push(appName);
-      log.info(`Found a ${appName} ${event.name} event`);
-      const amount = formatEther(event.args.value);
-      const deposit = tx.transfers.find(transfer =>
-        transfer.from === event.args.from && transfer.amount === amount
-      );
-      if (deposit) {
-        deposit.category = TransferCategories.Internal;
-        deposit.to = appAccount;
-        deposit.index = "index" in deposit ? deposit.index : txLog.index;
-        tx.method = "Deposit";
-      } else {
-        log.warn(`Couldn't find a ${appName} deposit of ${amount} ETH from ${event.args.from}`);
-      }
-
-    } else if (address === bjtjV2Address) {
-      const event = parseEvent(blackjackV2Abi, txLog, evmMeta);
-      if (!event.name) continue;
-      tx.apps.push(appName);
-      log.info(`Found a ${appName} ${event.name} event`);
-      if (event.name === "Deposit") {
-        const amount = formatEther(event.args.value);
-        const deposit = tx.transfers.find(transfer =>
-          transfer.from === event.args.from &&
-          transfer.to === bjtjV2Address &&
-          transfer.amount === amount
-        );
-        if (deposit) {
-          deposit.category = TransferCategories.Internal;
-          deposit.to = appAccount;
-          deposit.index = "index" in deposit ? deposit.index : txLog.index;
-          tx.method = "Deposit";
-        }
-      } else if (event.name === "Cashout") {
-        const amount = formatEther(event.args.value);
-        const withdraw = tx.transfers.find(transfer =>
-          transfer.to === event.args.to &&
-          transfer.amount === amount &&
-          transfer.from === bjtjV2Address
-        );
-        if (withdraw) {
-          withdraw.category = TransferCategories.Internal;
-          withdraw.from = appAccount;
-          withdraw.index = "index" in withdraw ? withdraw.index : txLog.index;
-          tx.method = "Withdraw";
-        }
-      }
-    }
-  }
 
   return tx;
 };
