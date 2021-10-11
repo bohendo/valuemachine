d9e98f6b38ba1aa19e39edb66d0e7d93afc4e24d
bohendo <bohendo@pm.me>
2021-10-11 11:54:33 -0400

    move INR-style commification to utils

---
+++ modules/utils/src/math.test.ts
@@ -0,0 +1,13 @@
+import { commify } from "./math";
+import { expect } from "./testUtils";
+
+describe("Math", () => {
+
+  it("should commify properly", async () => {
+    expect(commify("100000000")).to.equal("100,000,000.0");
+    expect(commify("-100000000")).to.equal("-100,000,000.0");
+    expect(commify("100000000", "INR")).to.equal("10,00,00,000.0");
+    expect(commify("-100000000", "INR")).to.equal("-10,00,00,000.0");
+  });
+
+});
