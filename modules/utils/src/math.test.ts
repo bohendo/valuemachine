60bab73741207a364bf9553da19948fc0b9e9f0c
bohendo <bohendo@pm.me>
2021-10-11 17:15:58 -0400

    fix tests

--- modules/utils/src/math.test.ts
+++ modules/utils/src/math.test.ts
@@ -6,8 +6,8 @@ describe("Math", () => {
   it("should commify properly", async () => {
     expect(commify("100000000")).to.equal("100,000,000.0");
     expect(commify("-100000000")).to.equal("-100,000,000.0");
-    expect(commify("100000000", "INR")).to.equal("10,00,00,000.0");
-    expect(commify("-100000000", "INR")).to.equal("-10,00,00,000.0");
+    expect(commify("100000000", "INR")).to.equal("10,00,00,000.00");
+    expect(commify("-100000000", "INR")).to.equal("-10,00,00,000.00");
   });
 
 });
