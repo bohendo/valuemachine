7779f2d6db05aabb0125ace257725e6d35267c2d
bohendo <bohendo@pm.me>
2021-10-27 14:10:52 -0400

    init improved mapping type

--- modules/types/src/taxes.ts
+++ modules/types/src/taxes.ts
@@ -1,17 +1,42 @@
+import { Static, Type } from "@sinclair/typebox";
+
 import { Asset, DateString, DecString } from "./strings";
 import { EventType } from "./vm";
 
-export type TaxRow = {
-  date: DateString;
-  action: EventType; // subset: Trade or Income
-  amount: DecString;
-  asset: Asset;
-  price: DecString;
-  tags: string[];
-  value: DecString;
-  receiveDate: DateString;
-  receivePrice: DecString;
-  capitalChange: DecString;
-  cumulativeChange: DecString;
-  cumulativeIncome: DecString;
-};
+export const FieldTypes = {
+  String: "String",
+  Boolean: "Boolean",
+} as const;
+export const FieldType = Type.Enum(FieldTypes);
+export type FieldType = Static<typeof FieldType>;
+
+export const Mapping = Type.Array(Type.Intersect([
+  Type.Object({
+    nickname: Type.String(),
+    fieldType: Type.Literal(FieldTypes.String),
+    fieldName: Type.String(),
+  }),
+  Type.Object({
+    nickname: Type.String(),
+    fieldType: Type.Literal(FieldTypes.Boolean),
+    fielName: Type.String(),
+    checkmark: Type.String(), // value needed to check the checkbox
+  }),
+]));
+export type Mapping = Static<typeof Mapping>;
+
+export const TaxRow = Type.Object({
+  date: DateString,
+  action: EventType, // subset: Trade or Income
+  amount: DecString,
+  asset: Asset,
+  price: DecString,
+  tags: Type.Array(Type.String()),
+  value: DecString,
+  receiveDate: DateString,
+  receivePrice: DecString,
+  capitalChange: DecString,
+  cumulativeChange: DecString,
+  cumulativeIncome: DecString,
+});
+export type TaxRow = Static<typeof TaxRow>;
