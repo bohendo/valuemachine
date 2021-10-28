a960681eb6d96f1e79ec34e08402ea952e9cd73d
bohendo <bohendo@pm.me>
2021-10-27 21:39:42 -0400

    fix tax tests

--- modules/taxes/src/index.ts
+++ modules/taxes/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./constants";
 export * from "./mappings";
 export * from "./pdf";
+export * from "./rows";
 export * from "./usa";
 export * from "./utils";
--- modules/taxes/src/mappings.test.ts
+++ modules/taxes/src/mappings.test.ts
@@ -11,16 +11,13 @@ import { fetchUsaForm, fillForm, fillReturn } from "./pdf";
 import { getPdftk } from "./pdftk";
 import { getTestForm, getTestReturn, syncMapping } from "./utils";
 
-const log = getLogger("info", "Mappings");
+const log = getLogger("warn", "Mappings");
 const libs = { fs, execFile };
 const pdftk = getPdftk(libs);
 const root = path.join(__dirname, "..");
 
 const getEmptyPdfPath = (year, form) =>
   `${root}/forms/${year}/${form}.pdf`;
-
-const getMappingPath = (year, form) =>
-  `${root}/src/mappings/${year}/${form}.json`;
 
 const getMapping = async (year, form): Mapping => {
   if (MappingArchive[year][form]) return MappingArchive[year][form];
@@ -45,12 +42,12 @@ describe("Tax Form Mappings", () => {
     expect(mapping).to.be.ok;
   });
 
-  it.only(`should create any mapping files that don't exist yet`, async () => {
+  it.skip(`should create any mapping files that don't exist yet`, async () => {
     for (const year of Object.keys(TaxYears)) {
       for (const form of Object.keys(MappingArchive[year])) {
         const emptyPdf = getEmptyPdfPath(year, form);
         // const mapping = await getMapping(year, form);
-        const currentMapping = JSON.parse(fs.readFileSync(getMappingPath(year, form))) as any;
+        const currentMapping = MappingArchive[year][form];
         const ts = await pdftk.getInterface(emptyPdf, form.toUpperCase(), currentMapping);
         const mappingFileContent = ts
           + `\n\nexport const ${form} = `
@@ -67,15 +64,14 @@ describe("Tax Form Mappings", () => {
   it(`should create & fix one form mapping`, async () => {
     const [year, form] = [TaxYears.USA20, "f1040"];
     const mapping = await getMapping(year, form);
-    const mappingPath = getMappingPath(year, form);
     let currentMapping;
-    if (fs.existsSync(mappingPath)) {
-      currentMapping = JSON.parse(fs.readFileSync(mappingPath)) as any;
-      syncMapping(mapping, currentMapping);
+    if (MappingArchive[year]?.[form]) {
+      currentMapping = MappingArchive[year][form];
+      syncMapping(form, mapping, currentMapping);
     } else {
       currentMapping = mapping;
     }
-    log.info(`Mapping at ${mappingPath} has ${currentMapping.length} entries`);
+    log.info(`Mapping for ${year} ${form} has ${currentMapping.length} entries`);
     // fs.writeFileSync(mappingPath, mapping);
   });
 
@@ -84,15 +80,14 @@ describe("Tax Form Mappings", () => {
       if (!MappingArchive[year]) continue; // Skip if we don't support forms for this year
       for (const form of Object.keys(MappingArchive[year])) {
         const mapping = await getMapping(year, form);
-        const mappingPath = getMappingPath(year, form);
-        let currentMapping;
-        if (fs.existsSync(mappingPath)) {
-          currentMapping = JSON.parse(fs.readFileSync(mappingPath)) as any;
-          syncMapping(mapping, currentMapping);
+        let currentMapping ;
+        if (MappingArchive[year]?.[form]) {
+          currentMapping = MappingArchive[year][form];
+          syncMapping(form, mapping, currentMapping);
         } else {
           currentMapping = mapping;
         }
-        log.info(`Mapping at ${mappingPath} has ${currentMapping.length} entries`);
+        log.info(`Mapping for ${year} ${form} has ${currentMapping.length} entries`);
         // fs.writeFileSync(mappingPath, JSON.stringify(currentMapping, null, 2));
       }
     }
