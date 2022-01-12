7d6ea8b6607156ab51f94af8a8afc4f7c591264e
bohendo <bohendo@pm.me>
2022-01-12 11:35:51 -0500

    avoid exponential number strings while getting tax rows

--- modules/taxes/src/rows.ts
+++ modules/taxes/src/rows.ts
@@ -65,12 +65,12 @@ export const getTaxRows = async ({
       if (!evt.outputs) { console.warn(`Missing ${evt.type} outputs`, evt); continue; }
       rows.push(...evt.outputs.map(chunkIndex => {
         const chunk = vm.getChunk(chunkIndex);
-        const price = (prices.getPrice(date, chunk.asset, unit) || 0).toString();
+        const price = (prices.getPrice(date, chunk.asset, unit) || 0).toFixed(18);
         if (chunk.asset !== unit && price !== "0") {
           const amount = chunk.amount;
           const value = math.mul(amount, price);
           const receiveDate = getDate(chunk.history[0].date);
-          const receivePrice = (prices.getPrice(receiveDate, chunk.asset, unit) || 0).toString();
+          const receivePrice = (prices.getPrice(receiveDate, chunk.asset, unit) || 0).toFixed(18);
           const capitalChange = math.mul(amount, math.sub(price, receivePrice));
           return {
             date: date,
@@ -95,7 +95,7 @@ export const getTaxRows = async ({
       if (!evt.inputs) { console.warn(`Missing ${evt.type} inputs`, evt); continue; }
       rows.push(...evt.inputs.map(chunkIndex => {
         const chunk = vm.getChunk(chunkIndex);
-        const price = (prices.getPrice(date, chunk.asset, unit) || 0).toString();
+        const price = (prices.getPrice(date, chunk.asset, unit) || 0).toFixed(18);
         const income = math.mul(chunk.amount, price);
         return {
           date: date,
@@ -117,12 +117,12 @@ export const getTaxRows = async ({
       if (!evt.outputs) { console.warn(`Missing ${evt.type} outputs`, evt); continue; }
       rows.push(...evt.outputs.map(chunkIndex => {
         const chunk = vm.getChunk(chunkIndex);
-        const price = (prices.getPrice(date, chunk.asset, unit) || 0).toString();
+        const price = (prices.getPrice(date, chunk.asset, unit) || 0).toFixed(18);
         const value = math.mul(chunk.amount, price);
         const receiveDate = chunk.history[0]?.date.split("T")[0];
         let receivePrice, capitalChange;
         if (chunk.asset !== unit && price !== "0") {
-          receivePrice = (prices.getPrice(receiveDate, chunk.asset, unit) || 0).toString();
+          receivePrice = (prices.getPrice(receiveDate, chunk.asset, unit) || 0).toFixed(18);
           capitalChange = math.mul(chunk.amount, math.sub(price, receivePrice));
         } else {
           receivePrice = price;
