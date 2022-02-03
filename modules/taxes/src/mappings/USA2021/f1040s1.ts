35a04d4e20037246fedd8b30d217f8073f101bae
bohendo <bohendo@pm.me>
2022-02-03 13:54:04 -0500

    manually update field nicknames for s1 & s2 & s3

--- modules/taxes/src/mappings/USA2021/f1040s1.ts
+++ modules/taxes/src/mappings/USA2021/f1040s1.ts
@@ -11,58 +11,58 @@ export const f1040s1 = [
   {"nickname":"L5","fieldName":"form1[0].Page1[0].f1_08[0]"},
   {"nickname":"L6","fieldName":"form1[0].Page1[0].f1_09[0]"},
   {"nickname":"L7","fieldName":"form1[0].Page1[0].f1_10[0]"},
-  {"nickname":"11_f1_11","fieldName":"form1[0].Page1[0].f1_11[0]"},
-  {"nickname":"12_f1_12","fieldName":"form1[0].Page1[0].f1_12[0]"},
-  {"nickname":"L8","fieldName":"form1[0].Page1[0].f1_13[0]"},
-  {"nickname":"L9","fieldName":"form1[0].Page1[0].f1_14[0]"},
-  {"nickname":"L10","fieldName":"form1[0].Page1[0].f1_15[0]"},
-  {"nickname":"L11","fieldName":"form1[0].Page1[0].f1_16[0]"},
-  {"nickname":"L12","fieldName":"form1[0].Page1[0].f1_17[0]"},
-  {"nickname":"L13","fieldName":"form1[0].Page1[0].f1_18[0]"},
-  {"nickname":"L14","fieldName":"form1[0].Page1[0].f1_19[0]"},
-  {"nickname":"L15","fieldName":"form1[0].Page1[0].f1_20[0]"},
-  {"nickname":"L16","fieldName":"form1[0].Page1[0].f1_21[0]"},
-  {"nickname":"L17","fieldName":"form1[0].Page1[0].f1_22[0]"},
-  {"nickname":"L18a","fieldName":"form1[0].Page1[0].f1_23[0]"},
-  {"nickname":"24_f1_24","fieldName":"form1[0].Page1[0].f1_24[0]"},
-  {"nickname":"L18c","fieldName":"form1[0].Page1[0].f1_25[0]"},
-  {"nickname":"L19","fieldName":"form1[0].Page1[0].f1_26[0]"},
-  {"nickname":"27_f1_27","fieldName":"form1[0].Page1[0].Line8z_ReadOrder[0].f1_27[0]"},
-  {"nickname":"28_f1_28","fieldName":"form1[0].Page1[0].Line8z_ReadOrder[0].f1_28[0]"},
-  {"nickname":"L22","fieldName":"form1[0].Page1[0].f1_29[0]"},
-  {"nickname":"30_f1_30","fieldName":"form1[0].Page1[0].f1_30[0]"},
-  {"nickname":"31_f1_31","fieldName":"form1[0].Page1[0].f1_31[0]"},
-  {"nickname":"32_f2_01","fieldName":"form1[0].Page2[0].f2_01[0]"},
-  {"nickname":"33_f2_02","fieldName":"form1[0].Page2[0].f2_02[0]"},
-  {"nickname":"34_f2_03","fieldName":"form1[0].Page2[0].f2_03[0]"},
-  {"nickname":"35_f2_04","fieldName":"form1[0].Page2[0].f2_04[0]"},
-  {"nickname":"36_f2_05","fieldName":"form1[0].Page2[0].f2_05[0]"},
-  {"nickname":"37_f2_06","fieldName":"form1[0].Page2[0].f2_06[0]"},
-  {"nickname":"38_f2_07","fieldName":"form1[0].Page2[0].f2_07[0]"},
-  {"nickname":"39_f2_08","fieldName":"form1[0].Page2[0].f2_08[0]"},
-  {"nickname":"40_f2_09","fieldName":"form1[0].Page2[0].f2_09[0]"},
-  {"nickname":"41_f2_10","fieldName":"form1[0].Page2[0].Line19b_CombField[0].f2_10[0]"},
-  {"nickname":"42_f2_11","fieldName":"form1[0].Page2[0].f2_11[0]"},
-  {"nickname":"43_f2_12","fieldName":"form1[0].Page2[0].f2_12[0]"},
-  {"nickname":"44_f2_13","fieldName":"form1[0].Page2[0].f2_13[0]"},
-  {"nickname":"45_f2_14","fieldName":"form1[0].Page2[0].f2_14[0]"},
-  {"nickname":"46_f2_15","fieldName":"form1[0].Page2[0].f2_15[0]"},
-  {"nickname":"47_f2_16","fieldName":"form1[0].Page2[0].f2_16[0]"},
-  {"nickname":"48_f2_17","fieldName":"form1[0].Page2[0].f2_17[0]"},
-  {"nickname":"49_f2_18","fieldName":"form1[0].Page2[0].f2_18[0]"},
-  {"nickname":"50_f2_19","fieldName":"form1[0].Page2[0].f2_19[0]"},
-  {"nickname":"51_f2_20","fieldName":"form1[0].Page2[0].f2_20[0]"},
-  {"nickname":"52_f2_21","fieldName":"form1[0].Page2[0].f2_21[0]"},
-  {"nickname":"53_f2_22","fieldName":"form1[0].Page2[0].f2_22[0]"},
-  {"nickname":"54_f2_23","fieldName":"form1[0].Page2[0].f2_23[0]"},
-  {"nickname":"55_f2_24","fieldName":"form1[0].Page2[0].f2_24[0]"},
-  {"nickname":"56_f2_25","fieldName":"form1[0].Page2[0].f2_25[0]"},
-  {"nickname":"57_f2_26","fieldName":"form1[0].Page2[0].f2_26[0]"},
-  {"nickname":"58_f2_27","fieldName":"form1[0].Page2[0].Line24z_ReadOrder[0].f2_27[0]"},
-  {"nickname":"59_f2_28","fieldName":"form1[0].Page2[0].Line24z_ReadOrder[0].f2_28[0]"},
-  {"nickname":"60_f2_29","fieldName":"form1[0].Page2[0].f2_29[0]"},
-  {"nickname":"61_f2_30","fieldName":"form1[0].Page2[0].f2_30[0]"},
-  {"nickname":"62_f2_31","fieldName":"form1[0].Page2[0].f2_31[0]"}
+  {"nickname":"L8a","fieldName":"form1[0].Page1[0].f1_11[0]"},
+  {"nickname":"L8b","fieldName":"form1[0].Page1[0].f1_12[0]"},
+  {"nickname":"L8c","fieldName":"form1[0].Page1[0].f1_13[0]"},
+  {"nickname":"L8d","fieldName":"form1[0].Page1[0].f1_14[0]"},
+  {"nickname":"L8e","fieldName":"form1[0].Page1[0].f1_15[0]"},
+  {"nickname":"L8f","fieldName":"form1[0].Page1[0].f1_16[0]"},
+  {"nickname":"L8g","fieldName":"form1[0].Page1[0].f1_17[0]"},
+  {"nickname":"L8h","fieldName":"form1[0].Page1[0].f1_18[0]"},
+  {"nickname":"L8i","fieldName":"form1[0].Page1[0].f1_19[0]"},
+  {"nickname":"L8j","fieldName":"form1[0].Page1[0].f1_20[0]"},
+  {"nickname":"L8k","fieldName":"form1[0].Page1[0].f1_21[0]"},
+  {"nickname":"L8l","fieldName":"form1[0].Page1[0].f1_22[0]"},
+  {"nickname":"L8m","fieldName":"form1[0].Page1[0].f1_23[0]"},
+  {"nickname":"L8n","fieldName":"form1[0].Page1[0].f1_24[0]"},
+  {"nickname":"L8o","fieldName":"form1[0].Page1[0].f1_25[0]"},
+  {"nickname":"L8p","fieldName":"form1[0].Page1[0].f1_26[0]"},
+  {"nickname":"L8z_Etc","fieldName":"form1[0].Page1[0].Line8z_ReadOrder[0].f1_27[0]"},
+  {"nickname":"L8z_Etc2","fieldName":"form1[0].Page1[0].Line8z_ReadOrder[0].f1_28[0]"},
+  {"nickname":"L8z","fieldName":"form1[0].Page1[0].f1_29[0]"},
+  {"nickname":"L9","fieldName":"form1[0].Page1[0].f1_30[0]"},
+  {"nickname":"L10","fieldName":"form1[0].Page1[0].f1_31[0]"},
+  {"nickname":"L11","fieldName":"form1[0].Page2[0].f2_01[0]"},
+  {"nickname":"L12","fieldName":"form1[0].Page2[0].f2_02[0]"},
+  {"nickname":"L13","fieldName":"form1[0].Page2[0].f2_03[0]"},
+  {"nickname":"L14","fieldName":"form1[0].Page2[0].f2_04[0]"},
+  {"nickname":"L15","fieldName":"form1[0].Page2[0].f2_05[0]"},
+  {"nickname":"L16","fieldName":"form1[0].Page2[0].f2_06[0]"},
+  {"nickname":"L17","fieldName":"form1[0].Page2[0].f2_07[0]"},
+  {"nickname":"L18","fieldName":"form1[0].Page2[0].f2_08[0]"},
+  {"nickname":"L19a","fieldName":"form1[0].Page2[0].f2_09[0]"},
+  {"nickname":"L19b","fieldName":"form1[0].Page2[0].Line19b_CombField[0].f2_10[0]"},
+  {"nickname":"L19c","fieldName":"form1[0].Page2[0].f2_11[0]"},
+  {"nickname":"L20","fieldName":"form1[0].Page2[0].f2_12[0]"},
+  {"nickname":"L21","fieldName":"form1[0].Page2[0].f2_13[0]"},
+  {"nickname":"L22","fieldName":"form1[0].Page2[0].f2_14[0]"},
+  {"nickname":"L23","fieldName":"form1[0].Page2[0].f2_15[0]"},
+  {"nickname":"L24a","fieldName":"form1[0].Page2[0].f2_16[0]"},
+  {"nickname":"L24b","fieldName":"form1[0].Page2[0].f2_17[0]"},
+  {"nickname":"L24c","fieldName":"form1[0].Page2[0].f2_18[0]"},
+  {"nickname":"L24d","fieldName":"form1[0].Page2[0].f2_19[0]"},
+  {"nickname":"L24e","fieldName":"form1[0].Page2[0].f2_20[0]"},
+  {"nickname":"L24f","fieldName":"form1[0].Page2[0].f2_21[0]"},
+  {"nickname":"L24g","fieldName":"form1[0].Page2[0].f2_22[0]"},
+  {"nickname":"L24h","fieldName":"form1[0].Page2[0].f2_23[0]"},
+  {"nickname":"L24i","fieldName":"form1[0].Page2[0].f2_24[0]"},
+  {"nickname":"L24j","fieldName":"form1[0].Page2[0].f2_25[0]"},
+  {"nickname":"L24k","fieldName":"form1[0].Page2[0].f2_26[0]"},
+  {"nickname":"L24z_Etc","fieldName":"form1[0].Page2[0].Line24z_ReadOrder[0].f2_27[0]"},
+  {"nickname":"L24z_Etc2","fieldName":"form1[0].Page2[0].Line24z_ReadOrder[0].f2_28[0]"},
+  {"nickname":"L24z","fieldName":"form1[0].Page2[0].f2_29[0]"},
+  {"nickname":"L25","fieldName":"form1[0].Page2[0].f2_30[0]"},
+  {"nickname":"L26","fieldName":"form1[0].Page2[0].f2_31[0]"}
 ];
 
 // This interface was automatically generated from the above schema
@@ -78,9 +78,25 @@ export interface F1040S1 {
   L5?: string;
   L6?: string;
   L7?: string;
-  "11_f1_11"?: string;
-  "12_f1_12"?: string;
-  L8?: string;
+  L8a?: string;
+  L8b?: string;
+  L8c?: string;
+  L8d?: string;
+  L8e?: string;
+  L8f?: string;
+  L8g?: string;
+  L8h?: string;
+  L8i?: string;
+  L8j?: string;
+  L8k?: string;
+  L8l?: string;
+  L8m?: string;
+  L8n?: string;
+  L8o?: string;
+  L8p?: string;
+  L8z_Etc?: string;
+  L8z_Etc2?: string;
+  L8z?: string;
   L9?: string;
   L10?: string;
   L11?: string;
@@ -90,44 +106,28 @@ export interface F1040S1 {
   L15?: string;
   L16?: string;
   L17?: string;
-  L18a?: string;
-  "24_f1_24"?: string;
-  L18c?: string;
-  L19?: string;
-  "27_f1_27"?: string;
-  "28_f1_28"?: string;
+  L18?: string;
+  L19a?: string;
+  L19b?: string;
+  L19c?: string;
+  L20?: string;
+  L21?: string;
   L22?: string;
-  "30_f1_30"?: string;
-  "31_f1_31"?: string;
-  "32_f2_01"?: string;
-  "33_f2_02"?: string;
-  "34_f2_03"?: string;
-  "35_f2_04"?: string;
-  "36_f2_05"?: string;
-  "37_f2_06"?: string;
-  "38_f2_07"?: string;
-  "39_f2_08"?: string;
-  "40_f2_09"?: string;
-  "41_f2_10"?: string;
-  "42_f2_11"?: string;
-  "43_f2_12"?: string;
-  "44_f2_13"?: string;
-  "45_f2_14"?: string;
-  "46_f2_15"?: string;
-  "47_f2_16"?: string;
-  "48_f2_17"?: string;
-  "49_f2_18"?: string;
-  "50_f2_19"?: string;
-  "51_f2_20"?: string;
-  "52_f2_21"?: string;
-  "53_f2_22"?: string;
-  "54_f2_23"?: string;
-  "55_f2_24"?: string;
-  "56_f2_25"?: string;
-  "57_f2_26"?: string;
-  "58_f2_27"?: string;
-  "59_f2_28"?: string;
-  "60_f2_29"?: string;
-  "61_f2_30"?: string;
-  "62_f2_31"?: string;
+  L23?: string;
+  L24a?: string;
+  L24b?: string;
+  L24c?: string;
+  L24d?: string;
+  L24e?: string;
+  L24f?: string;
+  L24g?: string;
+  L24h?: string;
+  L24i?: string;
+  L24j?: string;
+  L24k?: string;
+  L24z_Etc?: string;
+  L24z_Etc2?: string;
+  L24z?: string;
+  L25?: string;
+  L26?: string;
 }
--- modules/taxes/src/mappings/USA2021/f1040s2.ts
+++ modules/taxes/src/mappings/USA2021/f1040s2.ts
@@ -9,41 +9,41 @@ export const f1040s2 = [
   {"nickname":"L4","fieldName":"form1[0].Page1[0].f1_06[0]"},
   {"nickname":"L5","fieldName":"form1[0].Page1[0].f1_07[0]"},
   {"nickname":"L6","fieldName":"form1[0].Page1[0].f1_08[0]"},
-  {"nickname":"L7a","fieldName":"form1[0].Page1[0].f1_09[0]"},
-  {"nickname":"L7b","fieldName":"form1[0].Page1[0].f1_10[0]"},
-  {"nickname":"11_f1_11","fieldName":"form1[0].Page1[0].f1_11[0]"},
-  {"nickname":"L8","fieldName":"form1[0].Page1[0].f1_12[0]"},
-  {"nickname":"L9","fieldName":"form1[0].Page1[0].f1_13[0]"},
-  {"nickname":"L10","fieldName":"form1[0].Page1[0].f1_14[0]"},
-  {"nickname":"15_f1_15","fieldName":"form1[0].Page1[0].f1_15[0]"},
-  {"nickname":"16_f1_16","fieldName":"form1[0].Page1[0].f1_16[0]"},
-  {"nickname":"17_f1_17","fieldName":"form1[0].Page1[0].f1_17[0]"},
-  {"nickname":"18_f1_18","fieldName":"form1[0].Page1[0].f1_18[0]"},
-  {"nickname":"19_f2_01","fieldName":"form1[0].Page2[0].Line17a_ReadOrder[0].f2_01[0]"},
-  {"nickname":"20_f2_02","fieldName":"form1[0].Page2[0].f2_02[0]"},
-  {"nickname":"21_f2_03","fieldName":"form1[0].Page2[0].f2_03[0]"},
-  {"nickname":"22_f2_04","fieldName":"form1[0].Page2[0].f2_04[0]"},
-  {"nickname":"23_f2_05","fieldName":"form1[0].Page2[0].f2_05[0]"},
-  {"nickname":"24_f2_06","fieldName":"form1[0].Page2[0].f2_06[0]"},
-  {"nickname":"25_f2_07","fieldName":"form1[0].Page2[0].f2_07[0]"},
-  {"nickname":"26_f2_08","fieldName":"form1[0].Page2[0].f2_08[0]"},
-  {"nickname":"27_f2_09","fieldName":"form1[0].Page2[0].f2_09[0]"},
-  {"nickname":"28_f2_10","fieldName":"form1[0].Page2[0].f2_10[0]"},
-  {"nickname":"29_f2_11","fieldName":"form1[0].Page2[0].f2_11[0]"},
-  {"nickname":"30_f2_12","fieldName":"form1[0].Page2[0].f2_12[0]"},
-  {"nickname":"31_f2_13","fieldName":"form1[0].Page2[0].f2_13[0]"},
-  {"nickname":"32_f2_14","fieldName":"form1[0].Page2[0].f2_14[0]"},
-  {"nickname":"33_f2_15","fieldName":"form1[0].Page2[0].f2_15[0]"},
-  {"nickname":"34_f2_16","fieldName":"form1[0].Page2[0].f2_16[0]"},
-  {"nickname":"35_f2_17","fieldName":"form1[0].Page2[0].f2_17[0]"},
-  {"nickname":"36_f2_18","fieldName":"form1[0].Page2[0].f2_18[0]"},
-  {"nickname":"37_f2_19","fieldName":"form1[0].Page2[0].Line17z_ReadOrder[0].f2_19[0]"},
-  {"nickname":"38_f2_20","fieldName":"form1[0].Page2[0].Line17z_ReadOrder[0].f2_20[0]"},
-  {"nickname":"39_f2_21","fieldName":"form1[0].Page2[0].f2_21[0]"},
-  {"nickname":"40_f2_22","fieldName":"form1[0].Page2[0].f2_22[0]"},
-  {"nickname":"41_f2_23","fieldName":"form1[0].Page2[0].f2_23[0]"},
-  {"nickname":"42_f2_24","fieldName":"form1[0].Page2[0].f2_24[0]"},
-  {"nickname":"43_f2_25","fieldName":"form1[0].Page2[0].f2_25[0]"}
+  {"nickname":"L7","fieldName":"form1[0].Page1[0].f1_09[0]"},
+  {"nickname":"L8","fieldName":"form1[0].Page1[0].f1_10[0]"},
+  {"nickname":"L9","fieldName":"form1[0].Page1[0].f1_11[0]"},
+  {"nickname":"L10","fieldName":"form1[0].Page1[0].f1_12[0]"},
+  {"nickname":"L11","fieldName":"form1[0].Page1[0].f1_13[0]"},
+  {"nickname":"L12","fieldName":"form1[0].Page1[0].f1_14[0]"},
+  {"nickname":"L13","fieldName":"form1[0].Page1[0].f1_15[0]"},
+  {"nickname":"L14","fieldName":"form1[0].Page1[0].f1_16[0]"},
+  {"nickname":"L15","fieldName":"form1[0].Page1[0].f1_17[0]"},
+  {"nickname":"L16","fieldName":"form1[0].Page1[0].f1_18[0]"},
+  {"nickname":"L17a_Etc","fieldName":"form1[0].Page2[0].Line17a_ReadOrder[0].f2_01[0]"},
+  {"nickname":"L17a","fieldName":"form1[0].Page2[0].f2_02[0]"},
+  {"nickname":"L17b","fieldName":"form1[0].Page2[0].f2_03[0]"},
+  {"nickname":"L17c","fieldName":"form1[0].Page2[0].f2_04[0]"},
+  {"nickname":"L17d","fieldName":"form1[0].Page2[0].f2_05[0]"},
+  {"nickname":"L17e","fieldName":"form1[0].Page2[0].f2_06[0]"},
+  {"nickname":"L17f","fieldName":"form1[0].Page2[0].f2_07[0]"},
+  {"nickname":"L17g","fieldName":"form1[0].Page2[0].f2_08[0]"},
+  {"nickname":"L17h","fieldName":"form1[0].Page2[0].f2_09[0]"},
+  {"nickname":"L17i","fieldName":"form1[0].Page2[0].f2_10[0]"},
+  {"nickname":"L17j","fieldName":"form1[0].Page2[0].f2_11[0]"},
+  {"nickname":"L17k","fieldName":"form1[0].Page2[0].f2_12[0]"},
+  {"nickname":"L17l","fieldName":"form1[0].Page2[0].f2_13[0]"},
+  {"nickname":"L17m","fieldName":"form1[0].Page2[0].f2_14[0]"},
+  {"nickname":"L17n","fieldName":"form1[0].Page2[0].f2_15[0]"},
+  {"nickname":"L17o","fieldName":"form1[0].Page2[0].f2_16[0]"},
+  {"nickname":"L17p","fieldName":"form1[0].Page2[0].f2_17[0]"},
+  {"nickname":"L17q","fieldName":"form1[0].Page2[0].f2_18[0]"},
+  {"nickname":"L17z_Etc","fieldName":"form1[0].Page2[0].Line17z_ReadOrder[0].f2_19[0]"},
+  {"nickname":"L17z_Etc2","fieldName":"form1[0].Page2[0].Line17z_ReadOrder[0].f2_20[0]"},
+  {"nickname":"L17z","fieldName":"form1[0].Page2[0].f2_21[0]"},
+  {"nickname":"L18","fieldName":"form1[0].Page2[0].f2_22[0]"},
+  {"nickname":"L19","fieldName":"form1[0].Page2[0].f2_23[0]"},
+  {"nickname":"L20","fieldName":"form1[0].Page2[0].f2_24[0]"},
+  {"nickname":"L21","fieldName":"form1[0].Page2[0].f2_25[0]"}
 ];
 
 // This interface was automatically generated from the above schema
@@ -57,39 +57,39 @@ export interface F1040S2 {
   L4?: string;
   L5?: string;
   L6?: string;
-  L7a?: string;
-  L7b?: string;
-  "11_f1_11"?: string;
+  L7?: string;
   L8?: string;
   L9?: string;
   L10?: string;
-  "15_f1_15"?: string;
-  "16_f1_16"?: string;
-  "17_f1_17"?: string;
-  "18_f1_18"?: string;
-  "19_f2_01"?: string;
-  "20_f2_02"?: string;
-  "21_f2_03"?: string;
-  "22_f2_04"?: string;
-  "23_f2_05"?: string;
-  "24_f2_06"?: string;
-  "25_f2_07"?: string;
-  "26_f2_08"?: string;
-  "27_f2_09"?: string;
-  "28_f2_10"?: string;
-  "29_f2_11"?: string;
-  "30_f2_12"?: string;
-  "31_f2_13"?: string;
-  "32_f2_14"?: string;
-  "33_f2_15"?: string;
-  "34_f2_16"?: string;
-  "35_f2_17"?: string;
-  "36_f2_18"?: string;
-  "37_f2_19"?: string;
-  "38_f2_20"?: string;
-  "39_f2_21"?: string;
-  "40_f2_22"?: string;
-  "41_f2_23"?: string;
-  "42_f2_24"?: string;
-  "43_f2_25"?: string;
+  L11?: string;
+  L12?: string;
+  L13?: string;
+  L14?: string;
+  L15?: string;
+  L16?: string;
+  L17a_Etc?: string;
+  L17a?: string;
+  L17b?: string;
+  L17c?: string;
+  L17d?: string;
+  L17e?: string;
+  L17f?: string;
+  L17g?: string;
+  L17h?: string;
+  L17i?: string;
+  L17j?: string;
+  L17k?: string;
+  L17l?: string;
+  L17m?: string;
+  L17n?: string;
+  L17o?: string;
+  L17p?: string;
+  L17q?: string;
+  L17z_Etc?: string;
+  L17z_Etc2?: string;
+  L17z?: string;
+  L18?: string;
+  L19?: string;
+  L20?: string;
+  L21?: string;
 }
